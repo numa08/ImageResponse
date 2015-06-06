e0d0a894ee6ff55ec460af7296d66a42904491d9
numa08 <n511287@gmail.com>
2015-06-06 14:34:35 +0900

    initial commit

---
+++ app/src/main/java/numa08/net/imageresponse/MainActivity.kt
@@ -0,0 +1,20 @@
+package numa08.net.imageresponse
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+public class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        getMenuInflater().inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+}
